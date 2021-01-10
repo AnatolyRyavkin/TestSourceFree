e81180106f078e9883d9d61e1a016a994eff91d7
Рявкин Анатолий Владимирович <AVLRyavkin@sberbank.ru>
2021-01-10 22:07:57 +0300

    Initial Commit

---
+++ TestSourceFree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSourceFree
+//
+//  Created by 18855353 on 10/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
