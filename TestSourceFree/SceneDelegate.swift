573e4f98119202350e2158a5c2b1ba9be3964e9c
Рявкин Анатолий Владимирович <AVLRyavkin@sberbank.ru>
2021-01-11 02:57:34 +0300

    ds

--- TestSourceFree/SceneDelegate.swift
+++ TestSourceFree/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-		let viewController = ViewController(text: "hello")
+		let viewController = ViewController(text: "hello world")
 		viewController.view.backgroundColor = UIColor.lightGray
 		let navigationController = UINavigationController(rootViewController: viewController)
 		if let windowScene = scene as? UIWindowScene {
